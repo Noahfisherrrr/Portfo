27e5c3054bb8bdaecc1cf890ecf48ac4ad635484
Noah Kvirkvelia <kvirkvelianoah@gmail.com>
2025-07-18 17:35:01 +0400

    info card detail boxes update

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ const slides = document.querySelectorAll('.slide');
 const nextBtn = document.querySelector('.nextBtn');
 const prevBtn = document.querySelector('.prevBtn');
 
+const form = document.getElementById("hireForm");
+const hireMeBtn = document.getElementById("hireMeBtn");
+const modalHire = document.getElementById("hireMeModal");
+const closeBtn = document.querySelector(".close-btn");
+
+const skillsSection = document.querySelector('.skills');
+
 let currentSlideIndex = 0;
 let intervalId = null;
 
@@ -49,3 +56,35 @@ prevBtn.addEventListener('click', () => {
 
 slider.addEventListener('mouseenter', stopPlay);
 slider.addEventListener('mouseleave', autoPlay);
+
+// hireme modal
+hireMeBtn.addEventListener("click", () => {
+    modalHire.style.display = "flex"
+});
+
+closeBtn.addEventListener("click", () => {
+    modalHire.style.display = "none";
+    form.reset();
+
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modalHire) {
+        modalHire.style.display = "none";
+        form.reset();
+    }
+});
+
+skillsSection.addEventListener('mouseenter', () => {
+  document.querySelectorAll('.skill').forEach(skill => {
+    const fill = skill.querySelector('.fill');
+    const percent = skill.getAttribute('data-percent');
+    fill.style.width = percent + '%';
+  });
+});
+
+skillsSection.addEventListener('mouseleave', () => {
+  document.querySelectorAll('.fill').forEach(fill => {
+    fill.style.width = '0';
+  });
+});
